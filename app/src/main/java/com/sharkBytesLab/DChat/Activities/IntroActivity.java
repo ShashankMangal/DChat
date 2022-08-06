1d192cbb7cd25e74eb69f2da807eba3715f389b4
ShashankMangal <80992045+ShashankMangal@users.noreply.github.com>
2022-08-06 20:24:15 +0530

    intro activity complete commit

--- app/src/main/java/com/sharkBytesLab/DChat/Activities/IntroActivity.java
+++ app/src/main/java/com/sharkBytesLab/DChat/Activities/IntroActivity.java
@@ -20,19 +20,14 @@ public class IntroActivity extends MaterialIntroActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addSlide(new SlideFragmentBuilder().title("Title 1").description("First Intro screen").image(R.drawable.profile_image).buttonsColor(R.color.primaryDark).backgroundColor(R.color.primary).build());
+        addSlide(new SlideFragmentBuilder().title("Welcome to DChat").description("\nQuickly and easy to chat and messaging with your friends. Passing information on any screen, any device instantly.").image(R.drawable.ic_welcome_svg).buttonsColor(R.color.primaryDark).backgroundColor(R.color.primary).build());
 
-        addSlide(new SlideFragmentBuilder().title("Title 2").image(R.drawable.profile_image).buttonsColor(R.color.primaryDark).backgroundColor(R.color.primary).build());
+        addSlide(new SlideFragmentBuilder().title("Do real-time messaging").description("\nSend your information instantly with simplicity. The messages are End-To-End encrypted to secure your information.").image(R.drawable.ic_realtime_svg).buttonsColor(R.color.primaryDark).backgroundColor(R.color.primary).build());
 
-        addSlide(new SlideFragmentBuilder().title("Title 3").image(R.drawable.profile_image).buttonsColor(R.color.primaryDark).backgroundColor(R.color.primary).build(),
-                new MessageButtonBehaviour(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
+        addSlide(new SlideFragmentBuilder().title("Cloud Storage").description("\nYour messages are saved on cloud server securely, It won't get deleted anyhow.").image(R.drawable.ic_storage_svg).buttonsColor(R.color.primaryDark).backgroundColor(R.color.primary).build());
 
-                        showMessage("Intro Complete");
+        addSlide(new SlideFragmentBuilder().title("OK, Enough Intro").description("\nLet's chat").image(R.drawable.ic_start_svg).buttonsColor(R.color.primaryDark).backgroundColor(R.color.primary).build());
 
-                    }
-                }, "Work with love"));
 
     }
 }
