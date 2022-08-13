3e004199b12193193db3e3f326b12bb26ab2a461
ShashankMangal <80992045+ShashankMangal@users.noreply.github.com>
2022-08-13 12:07:41 +0530

    sign in complete without using Firebase Auth commit

--- app/src/main/java/com/sharkBytesLab/DChat/Activities/SignInActivity.java
+++ app/src/main/java/com/sharkBytesLab/DChat/Activities/SignInActivity.java
@@ -2,18 +2,35 @@ package com.sharkBytesLab.DChat.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.sharkBytesLab.DChat.Utilities.Constants;
+import com.sharkBytesLab.DChat.Utilities.PreferenceManager;
 import com.sharkBytesLab.DChat.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+
+        if(preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN))
+        {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -26,7 +43,84 @@ public class SignInActivity extends AppCompatActivity {
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
 
+        binding.signInButton.setOnClickListener(v -> {
+            if(isValidSignInDetails())
+            {
+                signIn();
+            }
+        });
+
     }
+
+    private void loading(Boolean isLoading)
+    {
+        if(isLoading)
+        {
+            binding.signInButton.setVisibility(View.INVISIBLE);
+            binding.signInProgressBar.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            binding.signInButton.setVisibility(View.VISIBLE);
+            binding.signInProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void signIn()
+    {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.signInEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.signInPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task ->
+                {
+                   if(task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() > 0)
+                   {
+                       DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                       preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                       preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                       preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+                       preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+                       Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                       startActivity(intent);
+                   }
+                   else
+                   {
+                       loading(false);
+                       showToast("Unable to Sign In!");
+                   }
+                });
+    }
+
+    private void showToast(String message)
+    {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidSignInDetails()
+    {
+        if(binding.signInEmail.getText().toString().trim().isEmpty())
+        {
+            binding.signInEmail.setError("Enter Email !");
+            return false;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(binding.signInEmail.getText().toString()).matches())
+        {
+            binding.signInEmail.setError("Enter valid Email !");
+            return false;
+        }
+        else if(binding.signInPassword.getText().toString().isEmpty())
+        {
+            binding.signInEmail.setError("Enter Password !");
+            return false;
+        }
+        return true;
+    }
+
+
 
 
 
