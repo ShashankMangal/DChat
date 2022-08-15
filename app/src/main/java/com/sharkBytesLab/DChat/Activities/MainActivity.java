cc70a53f83fe779272419e4b90b3521c45d11d36
ShashankMangal <80992045+ShashankMangal@users.noreply.github.com>
2022-08-15 23:59:04 +0530

    sign out and fcm token update in firebase commit

--- app/src/main/java/com/sharkBytesLab/DChat/Activities/MainActivity.java
+++ app/src/main/java/com/sharkBytesLab/DChat/Activities/MainActivity.java
@@ -3,17 +3,89 @@ package com.sharkBytesLab.DChat.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.sharkBytesLab.DChat.R;
+import com.sharkBytesLab.DChat.Utilities.Constants;
+import com.sharkBytesLab.DChat.Utilities.PreferenceManager;
+import com.sharkBytesLab.DChat.databinding.ActivityMainBinding;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
 
-        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.white));
+        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.primary));
+        loadUserDetails();
+        getToken();
+        setListeners();
     }
+
+    private void setListeners()
+    {
+        binding.mainImageSignOut.setOnClickListener(v -> signOut());
+    }
+
+    private void loadUserDetails()
+    {
+        binding.mainTextName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes= Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.mainImageProfile.setImageBitmap(bitmap);
+    }
+
+    private void showToast(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getToken()
+    {
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void updateToken(String token)
+    {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS).document(preferenceManager.getString(Constants.KEY_USER_ID));
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> showToast("Token Updated Successfully"))
+                .addOnFailureListener(e -> showToast("Unable to Update token"));
+    }
+
+    private void signOut()
+    {
+        showToast("Signing Out...");
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS).document(preferenceManager.getString(Constants.KEY_USER_ID));
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(unused -> {
+                    preferenceManager.clear();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(e -> showToast("Unable to Sign out !"));
+    }
+
+
 }
